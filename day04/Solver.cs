c3beec45d1e8b2da81d3fae9f05082f42da3768f
John Harper <john@harper.link>
2024-12-03 13:30:20 -0600

    Add regex to scaffolding

--- day04/Solver.cs
+++ day04/Solver.cs
@@ -3,17 +3,41 @@ namespace day04;
 using System.Text.RegularExpressions;
 using System.Linq;
 using System.Xml.Schema;
+using System.Security.Cryptography.X509Certificates;
 
 public class Solver
 {
     public long part1(string[] lines)
     {
-        return 0;
+        var input = parse(lines);
+        return input.Sum(l => l.Count);
     }
 
     public long part2(string[] lines)
     {
-        return 0;
+        var input = parse(lines);
+        return input.Sum(l => l.Count);
+    }
+
+    public List<List<string>> parse(string[] lines)
+    {
+        List<List<string>> result = new List<List<string>>();
+
+        string pattern = @"\d+";
+        Regex regex = new Regex(pattern);
+        foreach(string line in lines)
+        {
+            List<string> lineResult = new List<string>();
+            result.Add(lineResult);
+
+            MatchCollection matches = regex.Matches(line);
+            for (int i = 0; i < matches.Count; i++)
+            {
+                lineResult.Add(matches[i].Groups[0].Value);
+            }
+
+        }
+        return result;
     }
 
 }
