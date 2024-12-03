909fd5cc7aae36d77d57ac1e5178afd62ca1ee85
John Harper <john@harper.link>
2024-12-03 10:36:54 -0600

    scaffold day04

---
+++ day04/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+
+namespace day04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solver solver = new Solver();
+        string[] filenames = { "input-sample.txt", "input.txt" };
+
+        foreach (string filename in filenames)
+        {
+            execute(solver, filename);
+        }
+    }
+
+    static void execute(Solver solver, string filename)
+    {
+        string prefix = "-- ";
+        string suffix = " ";
+        int dividerLength = 40 - filename.Length - prefix.Length - suffix.Length;
+        string divider = new string('-', dividerLength);
+        Console.WriteLine($"{prefix}{filename}{suffix}{divider}");
+
+        string[] lines;
+
+        try
+        {
+            lines = File.ReadAllLines(filename);
+
+            var part1 = solver.part1(lines);
+            var part2 = solver.part2(lines);
+
+            Console.WriteLine($"part1: {part1}");
+            Console.WriteLine($"part2: {part2}");
+            Console.WriteLine();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+        }
+
+    }
+
+}
---
+++ day04/Solver.cs
@@ -0,0 +1,19 @@
+namespace day04;
+
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Xml.Schema;
+
+public class Solver
+{
+    public long part1(string[] lines)
+    {
+        return 0;
+    }
+
+    public long part2(string[] lines)
+    {
+        return 0;
+    }
+
+}
